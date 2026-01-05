5f777e8fe12c64f42612eccc221093dbfd003ffd
abraham <aanakagung@reapit.com>
2026-01-05 15:43:12 +0700

    [example] updating

--- examples/react-ts/src/App.tsx
+++ examples/react-ts/src/App.tsx
@@ -1,43 +1,119 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import * as React from "react";
 import { createDispatch, useDispatch } from "@rustyrush/dispatch";
 
-type Counter = {
-  count: number;
+import reactLogo from "./assets/react.svg";
+import dispatchLogo from "./assets/dispatch-logo.svg";
+import "./App.css";
+import type { DispatchEvents } from "../../../dist/vanilla";
+
+type TrafficLight = {
+  trafficLight: string;
 };
 
-const counter = createDispatch({
-  initialState: { count: 0 },
+const trafficLightAdmin = createDispatch<
+  TrafficLight,
+  DispatchEvents<TrafficLight>
+>({
+  initialState: { trafficLight: "red" },
   events: {
-    increment: (state: Counter) => ({ count: state.count + 1 }),
-    decrement: (state: Counter) => ({ count: state.count - 1 }),
+    toRed: () => ({ trafficLight: "red" }),
+    toYellow: () => ({ trafficLight: "yellow" }),
+    toGreen: () => ({ trafficLight: "green" }),
   },
   validNextEvents: {
-    increment: ["increment", "decrement"],
-    decrement: ["increment", "decrement"],
+    toRed: ["toYellow"],
+    toYellow: ["toGreen"],
+    toGreen: ["toRed"],
   },
 });
 
 function App() {
-  const state = useDispatch(counter);
+  const state = useDispatch(trafficLightAdmin);
+
+  const [running, setRunning] = React.useState(false);
+  const [error, setError] = React.useState("");
+
+  const handleLight = () => {
+    const currentLight = state.trafficLight;
+
+    if (!currentLight) return;
+
+    if (!running) setRunning(true);
+
+    switch (currentLight) {
+      case "red":
+        trafficLightAdmin.dispatch("toYellow");
+        break;
+      case "yellow":
+        trafficLightAdmin.dispatch("toGreen");
+        break;
+      case "green":
+        trafficLightAdmin.dispatch("toRed");
+        break;
+      default:
+        throw Error("Unknown state");
+    }
+  };
+
+  const oopsWrongEvent = () => {
+    const currentLight = state.trafficLight;
+
+    if (!currentLight) return;
+
+    try {
+      switch (currentLight) {
+        case "red":
+          trafficLightAdmin.dispatch("toRed");
+          break;
+        case "yellow":
+          trafficLightAdmin.dispatch("toRed");
+          break;
+        case "green":
+          trafficLightAdmin.dispatch("toYellow");
+          break;
+        default:
+          throw Error("Unknown state");
+      }
+    } catch (error) {
+      setError(error instanceof Error ? error.message : "You got me!");
+    }
+  };
 
   return (
     <>
       <div>
         <a href="https://github.com/padunk/dispatch" target="_blank">
-          <img src="" className="logo" alt="Dispatch logo" />
+          <img src={dispatchLogo} className="logo" alt="Dispatch logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Dispatch + React</h1>
+      <h1>Dispatch in React</h1>
       <div className="card">
-        <button onClick={() => counter.dispatch("increment")}>
-          count is {state.count}
-        </button>
+        <div className="traffic-light">
+          <div
+            className={`light red ${state.trafficLight !== "red" && "dim"}`}
+          ></div>
+          <div
+            className={`light yellow ${state.trafficLight !== "yellow" && "dim"}`}
+          ></div>
+          <div
+            className={`light green ${state.trafficLight !== "green" && "dim"}`}
+          ></div>
+        </div>
+        <div className="button-group">
+          <button onClick={handleLight}>Next Light</button>
+          {running && <button onClick={oopsWrongEvent}>Oops</button>}
+        </div>
+        {error && (
+          <>
+            <p className="error-text">Oops wrong event:</p>
+            <p className="error-text">{error}</p>
+          </>
+        )}
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test DispatchJS
         </p>
       </div>
       <p className="read-the-docs">
